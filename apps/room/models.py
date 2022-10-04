159dfbc274eda0e6a9077d16f1de4b7a17c3a6c7
Ljazz <elabdrasulov@gmail.com>
2022-10-04 11:01:30 +0600

    group, room, staff

--- apps/room/models.py
+++ apps/room/models.py
@@ -4,8 +4,15 @@ from django.db import models
 from apps.staff.models import Staff
 
 class Room(models.Model):
+    room_number = models.IntegerField()
+    
+    # Вместимость кабинета
     capacity = models.IntegerField()
+    
+    # Свободен ли кабинет
     room_status = models.BooleanField(default=False)
+
+
     mentor = models.ForeignKey(
         Staff, related_name='rooms', 
         on_delete=models.SET_DEFAULT,
@@ -13,3 +20,9 @@ class Room(models.Model):
     )
     # group = models.ForeignKey()
 
+    class Meta:
+        verbose_name = "Кабинет"
+        verbose_name_plural = "Кабинеты"
+
+    def __str__(self):
+        return f"Кабинет №{self.room_number} -> вместимость {self.capacity}"
--- apps/staff/models.py
+++ apps/staff/models.py
@@ -37,6 +37,13 @@ class Staff(models.Model):
         verbose_name="Конец стажировки", blank=True, null=True
     )
     
+    # заметки о сотруднике
+    notes = models.TextField(
+        max_length=1000, 
+        verbose_name="Дополнительно(заметки)",
+        blank=True, null=True
+    )
+
     # старт/конец работы трекером, ментором, куратором
     trackering_start_date = models.DateField(
         verbose_name="Начало работы трекером", blank=True
@@ -59,16 +66,6 @@ class Staff(models.Model):
         verbose_name="Конец работы куратором", blank=True
     )
 
-
-    # @property
-    # def start_date(self):
-    #     if self.staff_position == 'Tracker':
-    #         trackering_start_date = models.DateTimeField(
-    #             verbose_name="Начало работы трекером", blank=True
-    #         )
-    #         trackering_end_date = models.DateTimeField(
-    #             verbose_name="Конец работы трекером", blank=True
-    #         )
 
 
     class Meta:
